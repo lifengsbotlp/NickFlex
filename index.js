b470256a94da1ed28fb5e7cea14cd5e7f5bdab40
nick <nick@nickdeMBP.lan>
2018-11-06 18:17:38 +0800

    NickFlex 新移动适配方案发布

---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ * @author cuichuanteng 崔传腾
+ * @version 1.0.0 
+ */
+class NickFlex {
+    constructor({ width = 750, maxWidth = 500 } = {}) {
+        const html = document.documentElement;
+        this.getViewport = this.getViewport({ viewports: document.querySelectorAll('meta[name="viewport"]') });
+        this.resize = this.resize({ width, maxWidth, html, viewport: this.getViewport() });
+        this.resize();
+        window.addEventListener('resize', this.resize);
+    }
+    getViewport({ viewports }) {
+        const hasViewport = viewports.length;
+        const viewport = hasViewport ? viewports[viewports.length - 1] : document.createElement('meta');
+        if (!hasViewport) {
+            viewport.name = 'viewport';
+            document.querySelector('head').appendChild(viewport);
+        }
+        return () => viewport
+    }
+    resize({ width, maxWidth, viewport, html }) {
+        return () => {
+            viewport.content = 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0';
+            const clientWidth = html.clientWidth;
+            const deviceWidth = clientWidth > maxWidth ? maxWidth : clientWidth;
+            const scale = 1 / (width / deviceWidth);
+            const height = width / deviceWidth * html.clientHeight;
+            html.style.cssText = `
+                width:${width}px;
+                box-sizing:border-box;
+                transform-origin:left top;
+                transform:scale(${scale}) translateX(-50%);
+                position:absolute;
+                left:50%;
+                top:0;
+                height:${height}px;
+            `;
+        }
+    }
+}
+module.exports = NickFlex;
+// export default NickFlex
